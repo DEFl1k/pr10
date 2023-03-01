e65ebe8f2fb093154f3fb0854043c9b98f86191f
DEFl1k <igrogejmer@gmail.com>
2023-03-02 00:20:16 +0500

    first commit

---
+++ пр10/Cake.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр10
+{
+    internal class Cake
+    {
+        private string[] name;
+        private string[] testo;
+        private string[] krem;
+        private double[] ves;
+        private double[] kal;
+        private double[] cena;       
+        public void set_kol(int x)
+        {
+            name = new string[x];
+            testo = new string[x];
+            krem = new string[x];
+            ves = new double[x];
+            kal = new double[x];
+            cena = new double[x];
+        }
+        public void set_name(int i,string x)
+        {
+            name[i] = x;
+        }
+        public void set_testo(int i, string x)
+        {
+            testo[i] = x;
+        }
+        public void set_krem(int i, string x)
+        {
+            krem[i] = x;
+        }
+        public void set_ves(int i, double x)
+        {
+            ves[i] = x;
+        }
+        public void set_kal(int i, double x)
+        {
+            kal[i] = x;
+        }
+        public void set_cena(int i, double x)
+        {
+            cena[i] = x;
+        }
+        public void get_info(int y)
+        {
+            Console.WriteLine($"Название - {name[y - 1]}");
+            Console.WriteLine($"Тесто - {testo[y - 1]}");
+            Console.WriteLine($"Крем - {krem[y - 1]}");
+            Console.WriteLine($"Вес - {ves[y - 1]}");
+            Console.WriteLine($"Каллорийность - {kal[y-1]}");
+            Console.WriteLine($"Цена - {cena[y-1]}");
+        }
+        
+    }
+}
---
+++ пр10/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пр10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cake cake = new Cake();
+                Console.WriteLine("Сколько видов пирожных вы хотите записать?");
+                int x = int.Parse(Console.ReadLine());
+                while (x < 1)
+                {
+                    Console.WriteLine("Сколько видов пирожных вы хотите записать?");
+                    x = int.Parse(Console.ReadLine());
+                }
+                cake.set_kol(x);
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите название {i + 1} пирожного");
+                    string st = Console.ReadLine();
+                    while (st == "")
+                    {
+                        Console.WriteLine($"Введите название {i + 1} пирожного");
+                        st = Console.ReadLine();
+                    }
+                    cake.set_name(i, st);
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите тесто {i + 1} пирожного");
+                    string st = Console.ReadLine();
+                    while (st == "")
+                    {
+                        Console.WriteLine($"Введите тесто {i + 1} пирожного");
+                        st = Console.ReadLine();
+                    }
+                    cake.set_testo(i, st);
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите крем {i + 1} пирожного");
+                    string st = Console.ReadLine();
+                    while (st == "")
+                    {
+                        Console.WriteLine($"Введите крем {i + 1} пирожного");
+                        st = Console.ReadLine();
+                    }
+                    cake.set_krem(i, st);
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите вес {i + 1} пирожного");
+                    double st = Convert.ToDouble(Console.ReadLine());
+                    while (st < 0.1)
+                    {
+                        Console.WriteLine($"Введите вес {i + 1} пирожного");
+                        st = Convert.ToDouble(Console.ReadLine());
+                    }
+                    cake.set_ves(i, st);
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите каллорийность {i + 1} пирожного");
+                    double st = Convert.ToDouble(Console.ReadLine());
+                    while (st < 0.1)
+                    {
+                        Console.WriteLine($"Введите каллорийность {i + 1} пирожного");
+                        st = Convert.ToDouble(Console.ReadLine());
+                    }
+                    cake.set_kal(i, st);
+                }
+                for (int i = 0; i < x; i++)
+                {
+                    Console.WriteLine($"Введите цену {i + 1} пирожного");
+                    double st = Convert.ToDouble(Console.ReadLine());
+                    while (st < 0.1)
+                    {
+                        Console.WriteLine($"Введите цену {i + 1} пирожного");
+                        st = Convert.ToDouble(Console.ReadLine());
+                    }
+                    cake.set_cena(i, st);
+                }
+                Console.WriteLine("Информацию о каком пирожном вы хотите узнать?");
+                int y = int.Parse(Console.ReadLine());
+                while (y < 1)
+                {
+                    Console.WriteLine("Информацию о каком пирожном вы хотите узнать?");
+                    y = int.Parse(Console.ReadLine());
+                }
+                cake.get_info(y);
+            }
+            catch { Console.WriteLine("Неверный ввод"); }
+
+
+            Console.ReadKey();
+        }
+    }
+}
